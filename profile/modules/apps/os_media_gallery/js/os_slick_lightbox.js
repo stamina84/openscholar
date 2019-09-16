c64166075e3ff5ad9fe4e2c29ce4d00630310884
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-09-16 17:42:43 +0530

    #12319: added lightbox to media gallery

--- profile/modules/apps/os_media_gallery/js/os_slick_lightbox.js
+++ profile/modules/apps/os_media_gallery/js/os_slick_lightbox.js
@@ -1,13 +1,14 @@
 (function ($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.osSlickLightbox = {
-        attach: function (context) {
-            $('.slick--optionset--slick-media-gallery .slick-track').slickLightbox({
-                caption: 'caption'
-            });
-        }
-    };
+  Drupal.behaviors.osSlickLightbox = {
+    attach: function (context) {
+      $('.slick--optionset--slick-media-gallery .slick-track', context).slickLightbox({
+        caption: 'caption'
+      });
+      $('.field--name-field-gallery-media.field--mode-grid .field--items', context).slickLightbox();
+    }
+  };
 
 }(jQuery, Drupal));
