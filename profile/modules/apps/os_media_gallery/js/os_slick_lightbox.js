aa92e5bac96154bab56b04d0785e47549295e26b
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-09-18 15:58:39 +0530

    #12337: code added for media gallery carousel

--- profile/modules/apps/os_media_gallery/js/os_slick_lightbox.js
+++ profile/modules/apps/os_media_gallery/js/os_slick_lightbox.js
@@ -4,7 +4,7 @@
 
   Drupal.behaviors.osSlickLightbox = {
     attach: function (context) {
-      $('.slick--optionset--slick-media-gallery .slick-track', context).slickLightbox({
+      $('.slick--optionset--slick-media-gallery .slick-track').once('myCustomBehavior').slickLightbox({
         caption: 'caption'
       });
       $('.field--name-field-gallery-media.field--mode-grid .field--items', context).slickLightbox({
