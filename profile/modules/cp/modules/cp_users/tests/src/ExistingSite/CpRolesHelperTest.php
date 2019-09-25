33cca191ef063045114a2ffcb8d661c206aac8b9
Subhojit Paul <subhojitpaul21@gmail.com>
2019-09-25 10:54:13 +0530

    Issue #12330 - Fix failing testGetNonEditableGroupRoles

--- profile/modules/cp/modules/cp_users/tests/src/ExistingSite/CpRolesHelperTest.php
+++ profile/modules/cp/modules/cp_users/tests/src/ExistingSite/CpRolesHelperTest.php
@@ -35,7 +35,7 @@ class CpRolesHelperTest extends CpUsersExistingSiteTestBase {
     $cp_roles_helper = $this->container->get('cp_users.cp_roles_helper');
     $roles = $cp_roles_helper->getDefaultGroupRoles($this->group);
 
-    $this->assertCount(3, $roles);
+    $this->assertCount(4, $roles);
     $this->assertContains('personal-administrator', $roles);
     $this->assertContains('personal-member', $roles);
     $this->assertContains('personal-content_editor', $roles);
