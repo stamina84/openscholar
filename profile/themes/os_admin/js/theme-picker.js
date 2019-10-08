1eafc7aacbfb3fcdf28bc500d9bc4cc33232dd06
mukeyshkumar <mukeysh.kumar@axelerant.com>
2019-10-08 18:12:29 +0530

    Issue #12448 - fixed tabbing issue in theme selection

--- profile/themes/os_admin/js/theme-picker.js
+++ profile/themes/os_admin/js/theme-picker.js
@@ -1,15 +1,19 @@
 (function ($, Drupal) {
   Drupal.behaviors.theme_picker = {
     attach: function (context, settings) {
-      $('.theme-selector .theme-screenshot').click(function () {
-        let parent = $(this).parent();
-        $('.theme-selector').removeClass('checked');
-        if (!parent.hasClass('theme-default')) {
-          parent.addClass('checked');
+      $('.theme-selector').on('keydown click', function (e) {
+        if (e.which === 13 || e.type === 'click') {
+          console.log('d');
+          let parent = $(this);
+          $('.theme-selector').removeClass('checked');
+          if (!parent.hasClass('theme-default')) {
+            parent.addClass('checked');
+          }
+          var selectedTheme = parent.attr('data-attr');
+          $("#edit-theme").val(selectedTheme).change();
         }
-        var selectedTheme = parent.attr('data-attr');
-        $("#edit-theme").val(selectedTheme).change();
       });
+
     }
   };
 })(jQuery, Drupal);
