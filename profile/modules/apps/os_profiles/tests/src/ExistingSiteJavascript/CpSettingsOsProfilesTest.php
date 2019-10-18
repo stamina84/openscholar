0fbbadf2fae25036c91a4e1be672a45debd882a9
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-10-18 13:26:57 +0200

    Issue #11583 - Write test for removing file on settings form

--- profile/modules/apps/os_profiles/tests/src/ExistingSiteJavascript/CpSettingsOsProfilesTest.php
+++ profile/modules/apps/os_profiles/tests/src/ExistingSiteJavascript/CpSettingsOsProfilesTest.php
@@ -32,7 +32,7 @@ class CpSettingsOsProfilesTest extends OsExistingSiteJavascriptTestBase {
   public function setUp() {
     parent::setUp();
     $this->groupAdmin = $this->createUser();
-    $this->addGroupAdmin($this->groupAdmin, $this->group);
+    $this->addGroupEnhancedMember($this->groupAdmin, $this->group);
     $this->configFactory = $this->container->get('config.factory');
     $person = $this->createNode([
       'type' => 'person',
@@ -71,6 +71,43 @@ class CpSettingsOsProfilesTest extends OsExistingSiteJavascriptTestBase {
     $page = $this->getCurrentPage();
     $field_value = $page->findField('display_type')->getValue();
     $this->assertSame('no_image_teaser', $field_value, 'Form is not loaded api key value.');
+  }
+
+  /**
+   * Tests cp settings form submit and default value.
+   */
+  public function testCpSettingsFormRemoveFile() {
+    $this->container->get('vsite.context_manager')->activateVsite($this->group);
+    $web_assert = $this->assertSession();
+    $this->drupalLogin($this->groupAdmin);
+    $file = $this->createFile('image');
+    $file->setPermanent();
+    $file->save();
+    $profiles_config = $this->configFactory->getEditable('os_profiles.settings');
+    $profiles_config->set('default_image_fid', $file->id());
+    $profiles_config->save();
+
+    $this->visitViaVsite("cp/settings/apps-settings/profiles", $this->group);
+    $web_assert->statusCodeEquals(200);
+    $page = $this->getCurrentPage();
+    file_put_contents('public://testCpSettingsFormRemoveFile1.png', $this->getSession()->getScreenshot());
+    $file_remove_button = $page->findById('edit-default-image-fid-remove-button');
+    $this->assertNotNull($file_remove_button, 'Remove button is not found.');
+    $file_remove_button->press();
+    $web_assert->waitForElement('css', '.form-file');
+    $page->pressButton('Save configuration');
+    $page = $this->getCurrentPage();
+    $check_html_value = $page->hasContent('The configuration options have been saved.');
+    $this->assertTrue($check_html_value, 'The form is not saved.');
+
+    // Check form elements load default values.
+    $this->visitViaVsite("cp/settings/apps-settings/profiles", $this->group);
+    $web_assert->statusCodeEquals(200);
+    $page = $this->getCurrentPage();
+    $preview_container = $page->find('css', 'span.file--image');
+    $this->assertEmpty($preview_container, 'Image is not deleted from config.');
+    $profile_fid = $this->container->get('config.factory')->get('os_profiles.settings')->get('default_image_fid');
+    $this->assertNull($profile_fid);
   }
 
   /**
