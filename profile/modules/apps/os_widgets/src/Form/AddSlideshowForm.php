feabc91b4392d79bcd01f8409e2c46a92747a4bd
Balázs Sztreha <balazs.sztreha@cheppers.com>
2019-10-21 19:03:27 +0200

    Issue #12123 - Remove unnecessary code

--- profile/modules/apps/os_widgets/src/Form/AddSlideshowForm.php
+++ profile/modules/apps/os_widgets/src/Form/AddSlideshowForm.php
@@ -4,7 +4,6 @@ namespace Drupal\os_widgets\Form;
 
 use Drupal\block_content\BlockContentInterface;
 use Drupal\Component\Datetime\TimeInterface;
-use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\CloseModalDialogCommand;
 use Drupal\Core\Ajax\ReplaceCommand;
@@ -14,7 +13,6 @@ use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\vsite\Plugin\VsiteContextManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -121,34 +119,6 @@ class AddSlideshowForm extends ContentEntityForm {
     }
 
     return $response;
-  }
-
-  /**
-   * Checks access for access form.
-   *
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   Run access checks for this account.
-   * @param \Drupal\block_content\BlockContentInterface $block_content
-   *   Given block content.
-   *
-   * @return \Drupal\Core\Access\AccessResultInterface
-   *   The access result.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   */
-  public function access(AccountInterface $account, BlockContentInterface $block_content) {
-    if ($block_content->bundle() != 'slideshow') {
-      return AccessResult::forbidden([$this->t('Given block content is not a slideshow.')]);
-    }
-    $group_contents = $this->entityTypeManager->getStorage('group_content')->loadByEntity($block_content);
-    $group_content = array_shift($group_contents);
-    $block_content_group = $group_content->getGroup();
-    $group = $this->vsiteContextManager->getActiveVsite();
-    if ($group && $group->id() != $block_content_group->id()) {
-      return AccessResult::forbidden();
-    }
-    return AccessResult::allowed();
   }
 
 }
