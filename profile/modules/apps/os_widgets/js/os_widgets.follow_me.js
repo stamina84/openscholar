33f83fcc5576204f22fb2d8a40f8ac526cd7c1c6
chander-b <chander.b@axelerant.com>
2019-10-22 17:11:19 +0530

    Issue #12331 - Done PR review changes

--- profile/modules/apps/os_widgets/js/os_widgets.follow_me.js
+++ profile/modules/apps/os_widgets/js/os_widgets.follow_me.js
@@ -2,99 +2,100 @@
  * Allows users to add posts to their manual lists without an additional
  * page load on top of the ajax call
  */
-Drupal.behaviors.os_follow_me = {
-  attach: function (ctx, settings) {
+(function ($) {
+  Drupal.behaviors.os_follow_me = {
+    attach: function (ctx, settings) {
 
-    var $ = jQuery;
-  	if ($('#follow-links-list', ctx).length == 0) return;	// do nothing if our table doesn't exist
+      if ($('#follow-links-list', ctx).length == 0) return;	// do nothing if our table doesn't exist
 
-  	var $form = $('.os-follow-me-form'),
-  		template = '<tr class="draggable">'+$('input[name="links[blank][title]"]').parents('tr').hide().html()+'</tr>',
-  		tableDrag = Drupal.tableDrag['follow-links-list'],
-			count = $('input[type="hidden"][name="count"]'),
-			new_id = parseInt(count.val());
+      var $form = $('.os-follow-me-form'),
+        template = '<tr class="draggable">'+$('input[name="links[blank][title]"]').parents('tr').hide().html()+'</tr>',
+        tableDrag = Drupal.tableDrag['follow-links-list'],
+        count = $('input[type="hidden"][name="count"]'),
+        new_id = parseInt(count.val());
 
-  	// add a new row to the table, set all its form elements to the right values and make it draggable
-  	$('.add_new', $form).click(function (e) {
-  		var val = $('#edit-link-to-add', $form).val(),
-  			patt = /^https?:\/\/([^\/]+)/,
-  			matches = patt.exec(val),
-  			new_row, id, i, fd, weight = -Infinity;
-      // there should actually be something in the field
+      // add a new row to the table, set all its form elements to the right values and make it draggable
+      $('.add_new', $form).click(function (e) {
+        var edit_link_to_add = $('#edit-link-to-add', $form),
+          patt = /^https?:\/\/([^\/]+)/,
+          matches = patt.exec(edit_link_to_add.val()),
+          new_row, id, i, fd, weight = -Infinity;
+        // there should actually be something in the field
 
-  		if (matches != null) {
-  			var domain = matches[1],
-  				domains = drupalSettings.follow_networks;
+        if (matches != null) {
+          var domain = matches[1],
+            domains = drupalSettings.follow_networks;
 
-  			// get domain
-  			for (i in domains) {
-  				fd = domains[i];
-  				if (domain.indexOf(fd.domain) != -1) {
-  					domain = i;
-  					break;
-  				}
-  			}
+          // get domain
+          for (i in domains) {
+            fd = domains[i];
+            if (domain.indexOf(fd.domain) != -1) {
+              domain = i;
+              break;
+            }
+          }
 
-  			// if we don't have a valid domain, don't make a new row
-  			if (domain != matches[1]) {
-  				id = new_id++;
-  				new_row = $(template.replace(/blank/g, id));
-  				count.val(parseInt(count.val())+1);
+          // if we don't have a valid domain, don't make a new row
+          if (domain != matches[1]) {
+            id = new_id++;
+            new_row = $(template.replace(/blank/g, id));
+            count.val(parseInt(count.val())+1);
 
-  				// get the new weight
-  				$('.field-weight', $form).each(function () {
-  					if ($(this).val() > weight) {
-  						weight = parseInt($(this).val());
-  					}
-  				});
-  				// there are no existing form elements, start at 0.
-  				if (weight == -Infinity) {
-  					weight = 0;
-					}
+            // get the new weight
+            $('.field-weight', $form).each(function () {
+              if ($(this).val() > weight) {
+                weight = parseInt($(this).val());
+              }
+            });
+            // there are no existing form elements, start at 0.
+            if (weight == -Infinity) {
+              weight = 0;
+            }
 
-					// set all the form elements in the new row
-          $('span.rrssb-text', new_row).text(val);
-          $('li', new_row).addClass('rrssb-'+domain);
-  				$('input[name="links['+id+'][title]"]', new_row).val(val);
-  				$('input[name="links['+id+'][domain]"]', new_row).val(domain);
-					$('.default-weight', new_row).addClass('field-weight').val(weight+1);
-					$('.default-weight', new_row).parents('div').css('display', 'none');
-					$('#edit-links-'+id+'-weight', new_row).addClass('field-weight').val(weight+1);
-  				$('#edit-links-'+id+'-weight', new_row).parents('div').css('display', 'none');
-  				$('table tbody', $form).append(new_row);
-  				new_row = $('input[name="links['+id+'][title]"]', $form).parents('tr');
+            // set all the form elements in the new row
+            $('span.rrssb-text', new_row).text(val);
+            $('li', new_row).addClass('rrssb-'+domain);
+            $('input[name="links['+id+'][title]"]', new_row).val(val);
+            $('input[name="links['+id+'][domain]"]', new_row).val(domain);
+            $('.default-weight', new_row).addClass('field-weight').val(weight+1);
+            $('.default-weight', new_row).parents('div').css('display', 'none');
+            $('#edit-links-'+id+'-weight', new_row).addClass('field-weight').val(weight+1);
+            $('#edit-links-'+id+'-weight', new_row).parents('div').css('display', 'none');
+            $('table tbody', $form).append(new_row);
+            new_row = $('input[name="links['+id+'][title]"]', $form).parents('tr');
 
-  				setup_remove(new_row);
+            setup_remove( );
 
-          tableDrag.makeDraggable(new_row[0]);
+            tableDrag.makeDraggable(new_row[0]);
 
-  				// refreshes the variable
-  				$form = $('.os-follow-me-form');
-  			}
-  			else {
-  				// alert the user that the domain was not invalid.
-  				// bein' lazy for now
-  				alert(val+' is not from a valid social media domain.');
-  			}
-  		}
-  		else {
-  			alert(val+' is not from a valid social media domain.');
-  		}
-  		$('#edit-link-to-add', $form).val('');
-  	});
+            // refreshes the variable
+            $form = $('.os-follow-me-form');
+          }
+          else {
+            // alert the user that the domain was not invalid.
+            // bein' lazy for now
+            alert(edit_link_to_add.val() +' is not from a valid social media domain.');
+          }
+        }
+        else {
+          alert(edit_link_to_add.val() +' is not from a valid social media domain.');
+        }
+        edit_link_to_add.val('');
+      });
 
-  	// set up remove links.
-  	function setup_remove(ctx) {
-  		$('.remove', ctx).click(function () {
-  			var $this = $(this);
-  			$this.parents('tr').remove();
+      // set up remove links.
+      function setup_remove(ctx) {
+        $('.remove', ctx).click(function () {
+          var $this = $(this);
+          $this.parents('tr').remove();
 
-  			// decrement counter
-  			count.val(parseInt(count.val())-1);
+          // decrement counter
+          count.val(parseInt(count.val())-1);
 
-  			return false;
-  		});
-  	}
-  	setup_remove($form);
-  }
-};
+          return false;
+        });
+      }
+      setup_remove($form);
+    }
+  };
+})(jQuery);
