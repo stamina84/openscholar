f8e26c669bb32cf94b9fee29a8afdb5d9494f061
Subhojit Paul <subhojitpaul21@gmail.com>
2019-11-11 10:10:15 +0530

    #12331 follow me widget (#12568)
    * Issue #12331 - Added custom block type for follow me widget.
    
    * Issue #12331 - Added js for follow me widget.
    
    * Issue #12331 - Added code for follow me widget
    
    * Issue #12331 - Added code and js.
    
    * Issue #12331 - added rrssb-plus
    
    * Issue #12331 - added code and config files for follow me widget.
    
    * Issue #12331 - resolved conflict
    
    * Issue #12331 - added config files and module
    
    * Issue #12331 - Added follow me widget
    
    * Issue #12331 - Done some changes.
    
    * Issue #12331 - Re-order form fields.
    
    * Issue #12331 - Fixed draggable issue.
    
    * Issue #12331 - code added for follow me block
    
    * Issue #12331 - changed file name
    
    * Issue #12331 - Done PR review changes
    
    * Issue #12331 - Added Rss Feed page url.
    
    * Issue #12331 - Fixed PR failed config
    
    * Issue #12331 - Fixed PR review changes.
    
    * Issue #12331 -  Fixed PR review changes and library loading issue.
    
    * Issue #12331 - Done PR review changes.
    
    * Issue #12331 - Done PR review changes.
    
    * Issue #12331 - Fixed PR review changes and added test
    
    * Issue #12331 - Fixed PR review changes.
    
    * Issue #12331 - Fixed PR review changes.
    
    * Issue #12331 - Fixed PR review changes.
    
    * Issue #12331 - Fixed config for PR review changes.
    
    * Issue #12331 - fixed pr review points
    
    * Issue #12331 -
    
    * Issue #12331 - Modified tests for RssFeedBlock.

---
+++ profile/modules/apps/os_widgets/src/Plugin/OsWidgets/FollowMeWidget.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\os_widgets\Plugin\OsWidgets;
+
+use Drupal\os_widgets\OsWidgetsBase;
+use Drupal\os_widgets\OsWidgetsInterface;
+
+/**
+ * Class FollowMeWidget.
+ *
+ * @OsWidget(
+ *   id = "follow_me_widget",
+ *   title = @Translation("Follow Me")
+ * )
+ */
+class FollowMeWidget extends OsWidgetsBase implements OsWidgetsInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBlock(&$build, $block_content) {
+    $profile_links = os_widgets_get_profile_url_links($block_content);
+    unset($profile_links['links']['blank']);
+    $display_social = $block_content->get('field_display_social')->getString();
+    $rss_feed = $block_content->get('field_add_link_to_rss_feed_page')->getString();
+
+    $build['follow_me'] = [
+      '#theme' => 'os_widgets_follow_me',
+      '#profile_links' => $profile_links['links'],
+      '#display_social' => $display_social,
+      '#rss_feed' => $rss_feed,
+      '#rss_image_url' => '/' . drupal_get_path('theme', 'os_base') . '/images/rss.png',
+      '#rss_page_url' => '/rss',
+    ];
+
+    $build['follow_me']['#attached']['library'][] = 'os_widgets/followMeWidget';
+  }
+
+}
