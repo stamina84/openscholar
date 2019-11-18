0dbdd0436e45e62a61add45efa02c8be8770cf30
kiran <kiran.pratha@axelerant.com>
2019-11-18 17:49:20 +0530

    Issue #12590 - Upcoming calendar tests added.

---
+++ profile/modules/custom/os_fullcalendar/tests/src/ExistingSite/EventsCalendarTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\os_fullcalendar\ExistingSite;
+
+use Drupal\Component\Datetime\DateTimePlus;
+
+/**
+ * Tests mini calendar views.
+ *
+ * @group other
+ * @group kernel
+ *
+ * @covers UpcomingCalendar Events page.
+ */
+class EventsCalendarTest extends EventTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * Tests upcoming events calendar page.
+   */
+  public function testUpcomingEventsCalendarView(): void {
+    $web_assert = $this->assertSession();
+
+    $start = new DateTimePlus('tomorrow midnight', $this->user->getTimeZone());
+    $end = new DateTimePlus('2 day midnight', $this->user->getTimeZone());
+
+    /** @var \Drupal\node\NodeInterface $upcoming_event */
+    $upcoming_event = $this->createEvent([
+      'title' => 'Upcoming test event',
+      'field_recurring_date' => [
+        'value' => $start->format("Y-m-d\TH:i:s"),
+        'end_value' => $end->format("Y-m-d\TH:i:s"),
+        'rrule' => '',
+        'timezone' => $this->user->getTimeZone(),
+        'infinite' => FALSE,
+      ],
+      'field_location' => 'London',
+      'body' => 'Test body content',
+      'status' => TRUE,
+    ]);
+
+    $this->group->addContent($upcoming_event, "group_node:{$upcoming_event->bundle()}");
+    $this->visit("{$this->group->get('path')->first()->getValue()['alias']}/calendar/upcoming");
+    $web_assert->statusCodeEquals(200);
+    $this->assertSession()->pageTextContains('Upcoming test event');
+  }
+
+}
