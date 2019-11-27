7ee5c01c07cd3cc28c5c1dafe55f4f405880775f
Sumeet Pareek <positivecharge@gmail.com>
2019-11-27 11:21:28 -0500

    Issue #12451 - Injects FormBuilder instead of direct service call

--- profile/modules/apps/os_widgets/src/Plugin/OsWidgets/DataverseSearchBoxWidget.php
+++ profile/modules/apps/os_widgets/src/Plugin/OsWidgets/DataverseSearchBoxWidget.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\os_widgets\Plugin\OsWidgets;
 
+use Drupal\Core\Database\Connection;
 use Drupal\os_widgets\OsWidgetsBase;
 use Drupal\os_widgets\OsWidgetsInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class DataverseSearchBoxWidget.
@@ -14,9 +18,39 @@ use Drupal\os_widgets\OsWidgetsInterface;
  * )
  */
 class DataverseSearchBoxWidget extends OsWidgetsBase implements OsWidgetsInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
   // TODO: check with team if we should make this drupal config instead
   // of the os_widgets module instead of a class constant here?
   const DATAVERSE_SEARCH_BASE_URL = "https://dataverse.harvard.edu/dataverse.xhtml";
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $connection, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $connection);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('database'),
+      $container->get('form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -36,7 +70,7 @@ class DataverseSearchBoxWidget extends OsWidgetsBase implements OsWidgetsInterfa
 
     $params['dataverse_search_baseurl'] = $this::DATAVERSE_SEARCH_BASE_URL;
 
-    $form = \Drupal::formBuilder()->getForm('Drupal\os_widgets\Form\DataverseSearchBoxForm', $params);
+    $form = $this->formBuilder->getForm('Drupal\os_widgets\Form\DataverseSearchBoxForm', $params);
     $build['dataverse_search_box_form'] = $form;
   }
 
