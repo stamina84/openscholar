53e057276fc59dcb1e1e897f9eaa5f5b3230ea93
saurabh-axl <58168562+saurabh-axl@users.noreply.github.com>
2019-12-06 11:25:30 +0530

    Issue #12603 - Removed revision information for os widgets (#12681)
    * Issue #12603 - Removed revision information for os widgets
    
    * Issue #12603 - Test case for revision log and PR changes

---
+++ profile/modules/apps/os_widgets/tests/src/ExistingSiteJavascript/WidgetsRevisionLogTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\Tests\os_widgets\ExistingSiteJavascript;
+
+use Drupal\Tests\openscholar\ExistingSiteJavascript\OsExistingSiteJavascriptTestBase;
+
+/**
+ * Tests os_widgets module.
+ *
+ * @group functional-javascript
+ * @group widgets
+ */
+class WidgetsRevisionLogTest extends OsExistingSiteJavascriptTestBase {
+
+  /**
+   * List of all available os widgets.
+   *
+   * @var array
+   *   Os Widgets Type.
+   */
+  protected $widgetTypes;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+    $group_admin = $this->createUser();
+    $this->addGroupAdmin($group_admin, $this->group);
+    $this->drupalLogin($group_admin);
+
+    $bundleInfo = $this->container->get('entity_type.bundle.info');
+
+    $entity_type_id = 'block_content';
+    $this->widgetTypes = $bundleInfo->getBundleInfo($entity_type_id);
+  }
+
+  /**
+   * Tests if os_widgets revision log exists.
+   */
+  public function testWidgetsRevisionLogForm() {
+    $web_assert = $this->assertSession();
+
+    $this->visitViaVsite("?block-place=1", $this->group);
+    $web_assert->statusCodeEquals(200);
+
+    $page = $this->getCurrentPage();
+    $page->pressButton('Create New Widget');
+    // Iterate through all of widgets to check if revision information.
+    foreach ($this->widgetTypes as $key => $label) {
+      $class = str_replace('_', '-', $key);
+      $page->find('xpath', '//li[contains(@class, "' . $class . '")]/a')->press();
+      $web_assert->waitForText('Add new "' . $label['label'] . '" Widget');
+      $web_assert->pageTextNotContains('Revision information');
+      $web_assert->pageTextNotContains('Revision log message');
+      $page->find('xpath', '//button[contains(@class, "ui-dialog-titlebar-close")]')->press();
+    }
+  }
+
+}
