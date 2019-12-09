f1791dde15d7d86e52b540132d09442e9ae6b7f3
Uttkarsh Tiwari <38376852+uttkarsh-26@users.noreply.github.com>
2019-12-09 10:18:31 +0530

    12616 google tag manager (#12682)
    * Save.
    
    * Save.
    
    * Save.
    
    * Resolve merge conflicts.
    
    * Issue #12057 - GTM and settings.
    
    * Issue #12057 - GTM and settings.
    
    * Issue #12057 - Merge conflict
    
    * Issue #12616 - Form changes
    
    * Issue #12616 - Form test
    
    * Issue #12616 - Form test.
    
    * Issue #12616 - Tests and code refctoring.
    
    * Issue #12616 - Tests and code refctoring.
    
    * Issue #12616 - Code refactoring.
    
    * Issue #12616 - Do not allow admin pages to be tracked.
    
    * Issue #12616 - Code refactoring.
    
    * Issue #12616 - Test fix.

--- profile/modules/os/modules/os_google_analytics/src/Plugin/CpSetting/GaSettingForm.php
+++ profile/modules/os/modules/os_google_analytics/src/Plugin/CpSetting/GaSettingForm.php
@@ -3,7 +3,6 @@
 namespace Drupal\os_google_analytics\Plugin\CpSetting;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\cp_settings\CpSettingBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -23,8 +22,6 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class GaSettingForm extends CpSettingBase {
 
-  use StringTranslationTrait;
-
   /**
    * {@inheritdoc}
    */
@@ -40,27 +37,21 @@ class GaSettingForm extends CpSettingBase {
   public function getForm(array &$form, FormStateInterface $formState, ConfigFactoryInterface $configFactory) {
     $config_ga = $configFactory->get('os_ga.settings');
 
-    $description = $this->t('This ID is unique to each site you want 
-       to track separately, and is in the form of UA-xxxxxxx-yy. To get a Web Property 
+    $description = $this->t('This ID is unique to each site you want
+       to track separately, and is in the form of UA-xxxxxxx-yy. To get a Web Property
        ID, <a href="@analytics">register your site with Google Analytics</a>, or if you
        already have registered your site, go to your Google Analytics Settings page to
-       see the ID next to every site profile. <a href="@webpropertyid">Find more 
+       see the ID next to every site profile. <a href="@webpropertyid">Find more
        information in the documentation</a>.', [
          '@analytics' => 'https://marketingplatform.google.com/about/analytics/'
          , '@webpropertyid' => Url::fromUri('https://developers.google.com/analytics/resources/concepts/gaConceptsAccounts', ['fragment' => 'webProperty'])->toString(),
        ]);
-
-    $form['title'] = [
-      '#type' => 'page_title',
-      '#title' => $this->t('Google Analytics'),
-    ];
 
     $form['web_property_id'] = [
       '#default_value' => $config_ga->get('web_property_id'),
       '#description' => $description,
       '#maxlength' => 20,
       '#placeholder' => 'UA-',
-      '#required' => TRUE,
       '#size' => 20,
       '#title' => $this->t('Web Property ID'),
       '#type' => 'textfield',
@@ -71,6 +62,9 @@ class GaSettingForm extends CpSettingBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!$form_state->getValue('web_property_id')) {
+      return;
+    }
     parent::validateForm($form, $form_state);
     // Replace all type of dashes (n-dash, m-dash, minus) with normal dashes.
     $form_state->setValue('web_property_id', str_replace([
