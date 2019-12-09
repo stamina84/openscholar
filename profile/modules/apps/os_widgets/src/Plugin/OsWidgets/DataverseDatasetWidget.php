301de2e517cfafb592d5012c203381eaa463be7d
Balázs Sztreha <bsztreha@pfnet.hu>
2019-12-09 05:13:17 +0100

    Issue #12453 - Implement Dataverse Dataset widget and test (#12688)
    * Issue #12453 - Implement Dataverse Dataset widget and test
    
    * Issue #12453 - Implement integer embed height
    
    * Issue #12453 - Fix test
    
    * Issue #12453 - Field is integer, no need to parse
    
    * Issue #12453 - Add comment

---
+++ profile/modules/apps/os_widgets/src/Plugin/OsWidgets/DataverseDatasetWidget.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\os_widgets\Plugin\OsWidgets;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Url;
+use Drupal\os_widgets\OsWidgetsBase;
+use Drupal\os_widgets\OsWidgetsInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class DataverseDatasetWidget.
+ *
+ * @OsWidget(
+ *   id = "dataverse_dataset_widget",
+ *   title = @Translation("Dataverse Dataset Widget")
+ * )
+ */
+class DataverseDatasetWidget extends OsWidgetsBase implements OsWidgetsInterface {
+
+  /**
+   * The config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $connection, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $connection);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('database'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBlock(&$build, $block_content) {
+
+    $dataverse_config = $this->configFactory->get('os_widgets.dataverse');
+    $base_url = $dataverse_config->get('base_url');
+    if (empty($base_url)) {
+      return;
+    }
+
+    $embed_height = $block_content->get('field_embed_height')->getString();
+    $persistent_id = $block_content->get('field_dataset_persistent_id')->getString();
+    $persistent_type = $block_content->get('field_dataset_persistent_type')->getString();
+
+    $options = [
+      'query' => [
+        'persistentId' => $persistent_type . ":" . trim($persistent_id),
+        'dvUrl' => $base_url,
+        'widget' => 'iframe',
+        'heightPx' => $embed_height,
+      ],
+      'absolute' => TRUE,
+    ];
+    $js_url = Url::fromUri($base_url . "resources/js/widgets.js", $options)->toString();
+    $build['embed_dataverse'] = [
+      '#theme' => 'os_widgets_dataverse_dataset',
+      '#js_url' => $js_url,
+    ];
+  }
+
+}
