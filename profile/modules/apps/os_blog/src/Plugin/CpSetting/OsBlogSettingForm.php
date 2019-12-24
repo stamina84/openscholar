c4efbd47ac90f3b392fe4714d942e99fa43f12fd
kiran <kiran.pratha@axelerant.com>
2019-12-24 14:18:08 +0530

    Issue #12525 - Added access check for Blog settings.

--- profile/modules/apps/os_blog/src/Plugin/CpSetting/OsBlogSettingForm.php
+++ profile/modules/apps/os_blog/src/Plugin/CpSetting/OsBlogSettingForm.php
@@ -2,10 +2,16 @@
 
 namespace Drupal\os_blog\Plugin\CpSetting;
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\cp_settings\CpSettingBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\vsite\Plugin\VsiteContextManagerInterface;
+use Drupal\os_app_access\AppAccessLevels;
 
 /**
  * CP setting.
@@ -25,6 +31,45 @@ class OsBlogSettingForm extends CpSettingBase {
   use StringTranslationTrait;
 
   /**
+   * Config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Creates a new CpSettingBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\vsite\Plugin\VsiteContextManagerInterface $vsite_context_manager
+   *   Vsite context manager.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config factory service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, VsiteContextManagerInterface $vsite_context_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $vsite_context_manager);
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+    $configuration,
+    $plugin_id,
+    $plugin_definition,
+    $container->get('vsite.context_manager'),
+    $container->get('config.factory')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getEditableConfigNames(): array {
@@ -37,7 +82,7 @@ class OsBlogSettingForm extends CpSettingBase {
    * {@inheritdoc}
    */
   public function getForm(array &$form, FormStateInterface $formState, ConfigFactoryInterface $configFactory) {
-    $blog_config = $configFactory->get('os_blog.settings');
+    $blog_config = $this->configFactory->get('os_blog.settings');
 
     $form['comment_type'] = [
       '#type' => 'radios',
@@ -70,11 +115,32 @@ class OsBlogSettingForm extends CpSettingBase {
    * {@inheritdoc}
    */
   public function submitForm(FormStateInterface $formState, ConfigFactoryInterface $configFactory) {
-    $blog_config = $configFactory->getEditable('os_blog.settings');
+    $blog_config = $this->configFactory->getEditable('os_blog.settings');
     $blog_config
       ->set('comment_type', $formState->getValue('comment_type'))
       ->set('disqus_shortname', $formState->getValue('disqus_shortname'))
       ->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access(AccountInterface $account): AccessResultInterface {
+    if (!$this->activeVsite) {
+      return AccessResult::forbidden();
+    }
+
+    if (!$this->activeVsite->hasPermission('access control panel', $account)) {
+      return AccessResult::forbidden();
+    }
+
+    $levels = $this->configFactory->get('os_app_access.access');
+    $access_level = (int) $levels->get('blog');
+    if ($access_level === AppAccessLevels::DISABLED) {
+      return AccessResult::forbidden('This App has been disabled.');
+    }
+
+    return AccessResult::allowed();
   }
 
 }
