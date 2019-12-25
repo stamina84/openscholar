b4916266109991d9a2a22b2cf2c20281ca6acb7f
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2019-12-25 12:04:11 +0530

    Issue #12638 - Search sort Widget Test and PR changes

---
+++ profile/modules/custom/os_search/tests/src/ExistingSite/SearchSortWidgetTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\Tests\os_search\ExistingSite;
+
+use Drupal\Tests\os_widgets\ExistingSite\OsWidgetsExistingSiteTestBase;
+
+/**
+ * Tests Search sort block content.
+ *
+ * @group kernel
+ * @group widgets
+ * @covers \Drupal\os_search\Plugin\OsWidgets\SearchSortWidgetTest
+ */
+class SearchSortWidgetTest extends OsWidgetsExistingSiteTestBase {
+
+  /**
+   * Test build function to display filter post.
+   */
+  public function testBuildDisplaySearchSort() {
+    $block_content = $this->createBlockContent([
+      'type' => 'search_sort',
+      'info' => [
+        'value' => 'Search Sort Title',
+      ],
+      'field_widget_title' => ['Search Sort Title'],
+    ]);
+    $this->group->addContent($block_content, 'group_entity:block_content');
+    $view_builder = $this->entityTypeManager
+      ->getViewBuilder('block_content');
+    $render = $view_builder->view($block_content);
+    $renderer = $this->container->get('renderer');
+    /** @var \Drupal\Core\Render\Markup $markup_array */
+    $markup = $renderer->renderRoot($render);
+    $this->assertContains('Search Sort Title', $markup->__toString());
+  }
+
+}
