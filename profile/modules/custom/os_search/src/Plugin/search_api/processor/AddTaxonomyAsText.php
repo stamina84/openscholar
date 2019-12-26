08c8b039a7d69fcc3abfd8074c6e8364b992f8ae
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2019-12-26 14:22:31 +0530

    Issue #12642 - Filter by taxonomy processor and facet config

---
+++ profile/modules/custom/os_search/src/Plugin/search_api/processor/AddTaxonomyAsText.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\os_search\Plugin\search_api\processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Adds the item's Taxonomy to the indexed data.
+ *
+ * @SearchApiProcessor(
+ *   id = "add_taxonomy_as_text",
+ *   label = @Translation("Custom Taxonomy (text) field"),
+ *   description = @Translation("Adds the item's taxonomy to the indexed data."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class AddTaxonomyAsText extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Custom Taxonomy (text)'),
+        'description' => $this->t('Custom Taxonomy for entity.'),
+        'type' => 'string',
+        'is_list' => FALSE,
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['custom_taxonomy'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+
+    $object = $item->getOriginalObject()->getValue();
+    $custom_bundle = $object->getEntityTypeId();
+
+    if ($custom_bundle == 'node') {
+      if ($object->hasField('field_taxonomy_terms')) {
+        $tids = $object->get('field_taxonomy_terms')->getValue();
+
+        $fields = $this->getFieldsHelper()->filterForPropertyPath($item->getFields(), NULL, 'custom_taxonomy');
+        foreach ($fields as $field) {
+          foreach ($tids as $tid) {
+            $field->addValue($tid['target_id']);
+          }
+        }
+      }
+    }
+  }
+
+}
