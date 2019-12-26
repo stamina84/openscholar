59270c40feedb0a7f3228d0ef79aed977b898fa4
Ashish Prabhakar <jashish24@gmail.com>
2019-12-26 14:00:16 +0530

    12630 search base (#12715)
    * Issue #Elastic - Elastic Search Container & Elastic Connector Settings
    
    * Issue #Elastic - Elastic Search with Pages
    
    * Issue #Elastic - Elastic-Search
    
    * Issue #Elastic - Moving Field with Processor
    
    * Issue #Elastic - Search By Group
    
    * Issue #12630 - Winding Up - Search
    
    * Issue #12630 - Remove Kibana
    
    * Issue #12630 - Without Group - Publication Fix
    
    * Issue #12630 - SpellCheck - Hit & Trial
    
    * Issue #12630 - Search Permission
    
    * Issue #12630 - Enabled Apps Support: WIP
    
    * Issue #12630 - Enabled Apps Support: WIP
    
    * Issue #12630 - Cron: Search Index
    
    * Issue #12630 - PR Changes & Type
    
    * Issue #12630 - Change Group ID to integer.
    
    * Issue #12630 - PR Changes
    
    * Issue #12630 - Push One time Configs - Search Server
    
    * Issue #12630 - search page css
    
    * Issue #12630 - search page ui fixes
    
    Co-authored-by: mukeyshkumar <47223502+mukeyshkumar@users.noreply.github.com>

---
+++ profile/modules/custom/os_search/src/Plugin/search_api/processor/AddBundleAsText.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\os_search\Plugin\search_api\processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Adds the item's Bundle to the indexed data.
+ *
+ * @SearchApiProcessor(
+ *   id = "add_bundle_as_text",
+ *   label = @Translation("Custom Bundle (text) field"),
+ *   description = @Translation("Adds the item's Bundle/Entity Type ID for non-nodes to the indexed data."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class AddBundleAsText extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Custom Bundle (text)'),
+        'description' => $this->t('Custom Bundle for entity.'),
+        'type' => 'string',
+        'is_list' => FALSE,
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['custom_search_bundle'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+
+    $object = $item->getOriginalObject()->getValue();
+    $custom_bundle = $object->getEntityTypeId();
+
+    if ($custom_bundle == 'node') {
+      $custom_bundle = $object->bundle();
+    }
+
+    if ($custom_bundle) {
+      $fields = $this->getFieldsHelper()->filterForPropertyPath($item->getFields(), NULL, 'custom_search_bundle');
+
+      foreach ($fields as $field) {
+        $field->addValue($custom_bundle);
+      }
+    }
+  }
+
+}
---
+++ profile/modules/custom/os_search/src/Plugin/search_api/processor/AddGroupAsText.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\os_search\Plugin\search_api\processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+use Drupal\group\Entity\GroupContent;
+
+/**
+ * Adds the item's Group to the indexed data.
+ *
+ * @SearchApiProcessor(
+ *   id = "add_group_as_text",
+ *   label = @Translation("Group (text) field"),
+ *   description = @Translation("Adds the item's Group to the indexed data."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class AddGroupAsText extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Group (text)'),
+        'description' => $this->t('Group for entity.'),
+        'type' => 'integer',
+        'is_list' => FALSE,
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['custom_search_group'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+
+    $object = $item->getOriginalObject()->getValue();
+    $groups = GroupContent::loadByEntity($object);
+
+    if ($groups) {
+      $groups = array_reverse($groups);
+      $group = array_pop($groups);
+      $group_id = (string) $group->getGroup()->id();
+      $fields = $this->getFieldsHelper()->filterForPropertyPath($item->getFields(), NULL, 'custom_search_group');
+
+      foreach ($fields as $field) {
+        $field->addValue($group_id);
+      }
+    }
+  }
+
+}
