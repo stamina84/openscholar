5be55c3c6c706f22f09dd0ad8a30707fac3c5c8a
Saurabh Pandit <58168562+saurabh-axl@users.noreply.github.com>
2019-12-27 10:02:03 +0530

    12638 search sorting (#12727)
    * Issue #Elastic - Elastic Search Container & Elastic Connector Settings
    
    * Issue #Elastic - Elastic Search with Pages
    
    * Issue #Elastic - Elastic-Search
    
    * Issue #Elastic - Moving Field with Processor
    
    * Issue #Elastic - Search By Group
    
    * Issue #12630 - Winding Up - Search
    
    * Issue #12630 - Remove Kibana
    
    * Issue #12630 - Without Group - Publication Fix
    
    * Issue #12630 - SpellCheck - Hit & Trial
    
    * Issue #12630 - Search Permission
    
    * Issue #12630 - Enabled Apps Support: WIP
    
    * Issue #12630 - Enabled Apps Support: WIP
    
    * Issue #12630 - Cron: Search Index
    
    * Issue #12630 - PR Changes & Type
    
    * Issue #12630 - Change Group ID to integer.
    
    * Issue #12638 - Added processor for title sort
    
    * Issue #12638 - Added processor for title sort
    
    * Issue #12638 - Added sorting for created date and type
    
    * Issue #12630 - PR Changes
    
    * Issue #12638 - Added config for search sort
    
    * Issue #12638 - Sort widget and PR changes
    
    * Issue #12638 - Added config for sort block type
    
    * Issue #12630 - Push One time Configs - Search Server
    
    * Issue #12630 - search page css
    
    * Issue #12638 - Created test case for Search sort widget
    
    * Issue #12638 - Created test case for Search sort widget
    
    * Issue #12638 - Search Sort PR changes
    
    * Issue #12630 - search page ui fixes
    
    * Issue #12638 - Search sort Widget Test and PR changes
    
    * Issue #12638 - Search Sort config change
    
    * Issue #12638 - Added processor for title sort
    
    * Issue #12638 - Added processor for title sort
    
    * Issue #12638 - Added sorting for created date and type
    
    * Issue #12638 - Added config for search sort
    
    * Issue #12638 - Sort widget and PR changes
    
    * Issue #12638 - Added config for sort block type
    
    * Issue #12638 - Created test case for Search sort widget
    
    * Issue #12638 - Created test case for Search sort widget
    
    * Issue #12638 - Search Sort PR changes
    
    * Issue #12638 - Search sort Widget Test and PR changes
    
    * Issue #12638 - Search Sort config change
    
    * Issue #12638 - Changed default sort for date
    
    Co-authored-by: mukeyshkumar <47223502+mukeyshkumar@users.noreply.github.com>

---
+++ profile/modules/custom/os_search/src/Plugin/OsWidgets/SearchSortWidget.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\os_search\Plugin\OsWidgets;
+
+use Drupal\os_widgets\OsWidgetsBase;
+use Drupal\os_widgets\OsWidgetsInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Class SearchSortWidget.
+ *
+ * @OsWidget(
+ *   id = "search_sort_widget",
+ *   title = @Translation("Sort By")
+ * )
+ */
+class SearchSortWidget extends OsWidgetsBase implements OsWidgetsInterface {
+
+  /**
+   * Search sort type.
+   */
+  const SORT_TYPE = ['title', 'type', 'date'];
+
+  /**
+   * Route Match service.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routeMatch;
+
+  /**
+   * Request Stack service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, Connection $connection, CurrentRouteMatch $route_match, RequestStack $request_stack) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $connection);
+    $this->routeMatch = $route_match;
+    $this->requestStack = $request_stack;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('database'),
+      $container->get('current_route_match'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildBlock(&$build, $block_content) {
+
+    $route_name = $this->routeMatch->getRouteName();
+    if (strpos($route_name, 'search_api_page') !== FALSE) {
+      $request = $this->requestStack->getCurrentRequest();
+      $query_params = $request->query->all();
+
+      $link_types = self::SORT_TYPE;
+      $items = [];
+      $sort_dir = [];
+      // Check if there is an exists sort param in query and flip the direction.
+      if ($query_params['sort']) {
+        if ($query_params['dir'] == 'ASC') {
+          $sort_dir[$query_params['sort']] = 'DESC';
+        }
+        else {
+          $sort_dir[$query_params['sort']] = 'ASC';
+        }
+      }
+
+      foreach ($link_types as $link_type) {
+        $query_params['sort'] = $link_type;
+        if ($query_params['sort'] == 'date') {
+          $query_params['dir'] = 'DESC';
+        }
+        else {
+          $query_params['dir'] = 'ASC';
+        }
+        if ($sort_dir[$link_type]) {
+          $query_params['dir'] = $sort_dir[$link_type];
+        }
+        $url = Url::fromRoute($route_name, $query_params);
+        $items[] = Link::fromTextAndUrl($this->t('@text', ['@text' => ucfirst($link_type)]), $url)->toString();
+      }
+
+      $build['link-list'] = [
+        '#theme' => 'item_list',
+        '#list_type' => 'ul',
+        '#title' => $this->t('Relevance'),
+        '#items' => $items,
+        '#cache' => [
+          'max-age' => 0,
+        ],
+      ];
+    }
+  }
+
+}
---
+++ profile/modules/custom/os_search/src/Plugin/search_api/processor/AddTypeasText.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\os_search\Plugin\search_api\processor;
+
+use Drupal\search_api\Datasource\DatasourceInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\search_api\Processor\ProcessorProperty;
+
+/**
+ * Adds the common type to the indexed data.
+ *
+ * @SearchApiProcessor(
+ *   id = "add_type_as_text",
+ *   label = @Translation("Custom Entity Type (text) field"),
+ *   description = @Translation("Adds common type for all entites to the indexed data."),
+ *   stages = {
+ *     "add_properties" = 0,
+ *   },
+ *   locked = true,
+ *   hidden = true,
+ * )
+ */
+class AddTypeasText extends ProcessorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions(DatasourceInterface $datasource = NULL) {
+    $properties = [];
+
+    if (!$datasource) {
+      $definition = [
+        'label' => $this->t('Custom Type (text)'),
+        'description' => $this->t('Common Type for all entities.'),
+        'type' => 'string',
+        'is_list' => FALSE,
+        'processor_id' => $this->getPluginId(),
+      ];
+      $properties['custom_type'] = new ProcessorProperty($definition);
+    }
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addFieldValues(ItemInterface $item) {
+
+    $object = $item->getOriginalObject()->getValue();
+    $custom_bundle = $object->getEntityTypeId();
+
+    $custom_type = '';
+    // Get bundle or type based on Entity Type.
+    if ($custom_bundle == 'node') {
+      $custom_type = $object->bundle();
+    }
+    if ($custom_bundle == 'bibcite_reference') {
+      $custom_type = $object->get('type')->getValue()[0]['target_id'];
+    }
+
+    $fields = $this->getFieldsHelper()->filterForPropertyPath($item->getFields(), NULL, 'custom_type');
+
+    foreach ($fields as $field) {
+      $field->addValue($custom_type);
+    }
+  }
+
+}
