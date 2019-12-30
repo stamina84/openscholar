0e8e0871965fd4bbd465db7ce66b286ab457f335
Subhojit Paul <subhojitpaul21@gmail.com>
2019-12-30 14:43:29 +0530

    Issue #10897 - Fix - Duplicate media rows in views

--- profile/modules/os/modules/os_media/src/Plugin/views/filter/MediaUsageFilter.php
+++ profile/modules/os/modules/os_media/src/Plugin/views/filter/MediaUsageFilter.php
@@ -3,6 +3,7 @@
 namespace Drupal\os_media\Plugin\views\filter;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\NodeInterface;
 use Drupal\views\Plugin\views\filter\FilterPluginBase;
 
 /**
@@ -30,6 +31,7 @@ class MediaUsageFilter extends FilterPluginBase {
    * {@inheritdoc}
    */
   public function query() {
+    // TODO: Restrict the operator to `LIKE`.
     if (empty($this->value)) {
       return;
     }
@@ -38,7 +40,33 @@ class MediaUsageFilter extends FilterPluginBase {
     $value = reset($value);
 
     if (!empty($value)) {
-      $this->query->addWhere('AND', 'node_field_data.title', "%{$value}%", 'LIKE');
+      // TODO: Move this inside a service.
+      /** @var \Drupal\node\NodeStorageInterface $node_storage */
+      $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+      /** @var \Drupal\Core\Entity\Query\QueryInterface $field_attached_media_query */
+      $field_attached_media_query = $node_storage->getQuery();
+
+      $field_attached_media_query->condition('status', NodeInterface::PUBLISHED)
+        ->condition('type', [
+          'software_project',
+          'news',
+          'faq',
+          'class',
+          'blog',
+          'page',
+          'events',
+        ], 'IN')
+        ->condition('title', "%{$value}%", 'LIKE');
+
+      $field_attached_media_usages = $node_storage->loadMultiple($field_attached_media_query->execute());
+
+      $media_entity_ids = array_map(static function (NodeInterface $node) {
+        return $node->get('field_attached_media')->first()->getValue()['target_id'];
+      }, $field_attached_media_usages);
+
+      // TODO: Do the same for other node+media fields.
+      // TODO: Do the same for reference+media field.
+      $this->query->addWhere('AND', 'media_field_data.mid', $media_entity_ids, 'IN');
     }
   }
 
