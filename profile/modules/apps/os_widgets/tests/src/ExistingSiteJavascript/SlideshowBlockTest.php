1edb7fbab39df8dc04c4b0a98d385e54eee5bc30
Balázs Sztreha <balazs.sztreha@cheppers.com>
2020-01-22 12:44:51 +0100

    Issue #12123 - Align test for new code and remove add slide media test

--- profile/modules/apps/os_widgets/tests/src/ExistingSiteJavascript/SlideshowBlockTest.php
+++ profile/modules/apps/os_widgets/tests/src/ExistingSiteJavascript/SlideshowBlockTest.php
@@ -47,36 +47,4 @@ class SlideshowBlockTest extends OsExistingSiteJavascriptTestBase {
     $web_assert->pageTextContains('Standard Side');
   }
 
-  /**
-   * Test add slideshow link modal form on page.
-   */
-  public function testAddSlideshowLinkVisibleAndShowModalForm() {
-    $web_assert = $this->assertSession();
-    $image = $this->createMediaImage();
-    $block_content = $this->createBlockContent([
-      'type' => 'slideshow',
-    ]);
-
-    $this->group->addContent($block_content, 'group_entity:block_content');
-    $this->group->addContent($image, 'group_entity:media');
-    $this->placeBlockContentToRegion($block_content, 'sidebar_second');
-
-    $this->visitViaVsite("", $this->group);
-    $page = $this->getCurrentPage();
-    $page->findLink('Add slide')->press();
-    $web_assert->waitForElement('css', '.modal-dialog');
-    $web_assert->pageTextContains('Add new slideshow');
-    $page->fillField('field_slide_image[0][target_id]', $image->label());
-    $result = $web_assert->waitForElementVisible('css', '.ui-autocomplete li');
-    $this->assertNotNull($result);
-    // Click the autocomplete option.
-    $result->click();
-    // Verify that correct the input is selected.
-    $web_assert->pageTextContains($image->label());
-    $submit_button = $page->findButton('Save');
-    $submit_button->press();
-    $new_slide = $web_assert->waitForElement('css', '.slide--0');
-    $this->assertNotNull($new_slide, 'New slide is not visible.');
-  }
-
 }
