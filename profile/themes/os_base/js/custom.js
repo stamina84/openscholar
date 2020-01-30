542ecf9af7820bc4250937e2e25062340ce5011d
mukeyshkumar <mukeysh.kumar@axelerant.com>
2020-01-30 14:47:58 +0530

    js changes

--- profile/themes/os_base/js/custom.js
+++ profile/themes/os_base/js/custom.js
@@ -1,20 +1,10 @@
 (function ($, Drupal) {
   Drupal.behaviors.custom = {
     attach: function (context, settings) {
-      $(".nav li.expanded").hover(
-        function(){
-          $(this).addClass("open");
-        },function(){
-          $(this).removeClass("open");
-        }
-      );
-      // mobile header toggle for search and secondary menu
-      // adding dummy classes needs to replace with actual blocks
-      $(".mobile-menu .search").click(function(){
-        $(".search-block").collapse('toggle');
-      });
-      $(".mobile-menu .secondary").click(function () {
-        $("nav.secondary").collapse('toggle');
+      $('.dropmenu-child', context).click(function (e) {
+        e.preventDefault();
+        $(this).siblings('.dropdown-menu').toggleClass("mopen");
+        $(this).toggleClass("mopen");
       });
     }
   };
