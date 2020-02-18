b8b637da48ac53b62cd3d85d3823e55734318651
Subhojit Paul <subhojitpaul21@gmail.com>
2020-02-18 16:33:41 +0530

    Issue #12991 - Alter custom theme specific query string

--- profile/modules/cp/modules/cp_appearance/src/CssCollectionRenderer.php
+++ profile/modules/cp/modules/cp_appearance/src/CssCollectionRenderer.php
@@ -3,6 +3,7 @@
 namespace Drupal\cp_appearance;
 
 use Drupal\Core\Asset\AssetCollectionRendererInterface;
+use Drupal\cp_appearance\Entity\CustomTheme;
 
 /**
  * Makes sure custom theme styles are only refreshed in browser.
@@ -30,8 +31,20 @@ class CssCollectionRenderer implements AssetCollectionRendererInterface {
    * {@inheritdoc}
    */
   public function render(array $assets): array {
-    // TODO: Alter the custom theme query string.
-    return $this->coreCssCollectionRenderer->render($assets);
+    $elements = $this->coreCssCollectionRenderer->render($assets);
+
+    // Only alter the query string for any custom theme resource.
+    array_walk($elements, static function (&$item, $index) use ($assets) {
+      if (strpos($item['#attributes']['href'], CustomTheme::CUSTOM_THEME_ID_PREFIX) !== FALSE) {
+        $query_string_separator = (strpos($assets[$index]['data'], '?') !== FALSE) ? '&' : '?';
+        list($path) = explode($query_string_separator, $item['#attributes']['href']);
+        // TODO: Use the custom state here.
+        $new_query_string = base_convert(REQUEST_TIME + 1, 10, 36);
+        $item['#attributes']['href'] = "$path$query_string_separator$new_query_string";
+      }
+    });
+
+    return $elements;
   }
 
 }
