303f8d6bc147c9ec171953d57b5cab99603c9b55
Saurabh Pandit <saurabhpandit@Saurabhs-MacBook-Pro.local>
2020-02-19 19:22:25 +0530

    Issue #12845 - Events CSV Import

--- profile/modules/apps/os_events/src/Plugin/App/EventsApp.php
+++ profile/modules/apps/os_events/src/Plugin/App/EventsApp.php
@@ -2,7 +2,14 @@
 
 namespace Drupal\os_events\Plugin\App;
 
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\Messenger;
+use Drupal\cp_import\AppImportFactory;
+use Drupal\cp_import\Helper\CpImportHelper;
+use Drupal\migrate\Plugin\MigrationPluginManager;
 use Drupal\vsite\Plugin\AppPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Events app.
@@ -29,9 +36,73 @@ use Drupal\vsite\Plugin\AppPluginBase;
  *     },
  *   },
  *   id = "event",
- *   contextualRoute = "view.upcoming_calendar.page"
+ *   contextualRoute = "view.upcoming_calendar.page",
+ *   cpImportId = "os_events_import",
+ *   cpImportFilePath = "public://importcsv/os_events.csv"
  * )
  */
 class EventsApp extends AppPluginBase {
+  /**
+   * App Import factory service.
+   *
+   * @var \Drupal\cp_import\AppImportFactory
+   */
+  protected $appImportFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationPluginManager $migrationPluginManager, CpImportHelper $cpImportHelper, Messenger $messenger, EntityTypeManager $entityTypeManager, AppImportFactory $appImportFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migrationPluginManager, $cpImportHelper, $messenger, $entityTypeManager);
+    $this->appImportFactory = $appImportFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.migration'),
+      $container->get('cp_import.helper'),
+      $container->get('messenger'),
+      $container->get('entity_type.manager'),
+      $container->get('app_import_factory')
+    );
+  }
+
+  /**
+   * Validates import file and shows errors row wise.
+   *
+   * @param array $form
+   *   The form.
+   * @param \Drupal\Core\Form\FormStateInterface $formState
+   *   The Form State.
+   */
+  public function validateImportSource(array $form, FormStateInterface $formState) {
+    $data = parent::validateImportSource($form, $formState);
+    if (!$data) {
+      return;
+    }
+
+    $definition = $this->getPluginDefinition();
+    $blogImport = $this->appImportFactory->create($definition['cpImportId']);
+
+    // Validate Headers.
+    if ($missing = $blogImport->validateHeaders($data)) {
+      $headerMessage = $this->t('The following Header/columns are missing:<ul> @Title @Body @Start date @End date @Location @Registration @Files @Created date @Path</ul></br> The structure of your CSV file probably needs to be updated. Please download the template again.', $missing);
+      $this->messenger->addError($headerMessage);
+      $formState->setError($form['import_file']);
+      return;
+    }
+
+    // Validate rows.
+    if ($message = $blogImport->validateRows($data)) {
+      $formState->setError($form['import_file']);
+      $this->messenger->addError($this->t('@title @file @date', $message));
+    }
+  }
 
 }
