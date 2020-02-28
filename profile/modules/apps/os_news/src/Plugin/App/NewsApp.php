579431043254914216fea52197e4deaaf11d8d82
Saurabh Pandit <58168562+saurabh-axl@users.noreply.github.com>
2020-02-28 18:26:02 +0530

    Issue #12844 - Import News (#13065)
    * Issue #12844 - Added Import template and migrate yaml for news
    
    * Issue #12844 - Added Import News migration and validation logic
    
    * Issue #12844 - Test cases for Import News
    
    * Issue #12844 - Code cleanup
    
    * Issue #12844 - CSV encoding issue
    
    * Issue #12844 - Travis test fix
    
    * Issue #12844 - PR Review Correction
    
    Co-authored-by: Richard Brandon <rbran100@users.noreply.github.com>

--- profile/modules/apps/os_news/src/Plugin/App/NewsApp.php
+++ profile/modules/apps/os_news/src/Plugin/App/NewsApp.php
@@ -2,7 +2,14 @@
 
 namespace Drupal\os_news\Plugin\App;
 
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\Messenger;
+use Drupal\cp_import\AppImportFactory;
+use Drupal\cp_import\Helper\CpImportHelper;
+use Drupal\migrate\Plugin\MigrationPluginManager;
 use Drupal\vsite\Plugin\AppPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * News app.
@@ -21,9 +28,74 @@ use Drupal\vsite\Plugin\AppPluginBase;
  *     },
  *   },
  *   id = "news",
- *   contextualRoute = "view.news.page_1"
+ *   contextualRoute = "view.news.page_1",
+ *   cpImportId = "os_news_import",
+ *   cpImportFilePath = "public://importcsv/os_news.csv"
  * )
  */
 class NewsApp extends AppPluginBase {
+
+  /**
+   * App Import factory service.
+   *
+   * @var \Drupal\cp_import\AppImportFactory
+   */
+  protected $appImportFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationPluginManager $migrationPluginManager, CpImportHelper $cpImportHelper, Messenger $messenger, EntityTypeManager $entityTypeManager, AppImportFactory $appImportFactory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migrationPluginManager, $cpImportHelper, $messenger, $entityTypeManager);
+    $this->appImportFactory = $appImportFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('plugin.manager.migration'),
+      $container->get('cp_import.helper'),
+      $container->get('messenger'),
+      $container->get('entity_type.manager'),
+      $container->get('app_import_factory')
+    );
+  }
+
+  /**
+   * Validates import file and shows errors row wise.
+   *
+   * @param array $form
+   *   The form.
+   * @param \Drupal\Core\Form\FormStateInterface $formState
+   *   The Form State.
+   */
+  public function validateImportSource(array $form, FormStateInterface $formState) {
+    $data = parent::validateImportSource($form, $formState);
+    if (!$data) {
+      return;
+    }
+
+    $definition = $this->getPluginDefinition();
+    $faqImport = $this->appImportFactory->create($definition['cpImportId']);
+
+    // Validate Headers.
+    if ($missing = $faqImport->validateHeaders($data)) {
+      $headerMessage = $this->t('The following Header/columns are missing:<ul> @Title @Date @Body @Redirect @Image @Files @Created date @Path</ul></br> The structure of your CSV file probably needs to be updated. Please download the template again.', $missing);
+      $this->messenger->addError($headerMessage);
+      $formState->setError($form['import_file']);
+      return;
+    }
+
+    // Validate rows.
+    if ($message = $faqImport->validateRows($data)) {
+      $formState->setError($form['import_file']);
+      $this->messenger->addError($this->t('@title @news_date @image @file @date', $message));
+    }
+  }
 
 }
