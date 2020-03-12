80e59225c3026e8b3a6f823a64fd26e31c92f6d3
Lomas Rishi Gupta <lomasgupta35@gmail.com>
2020-03-12 18:25:25 +0530

    Issue #13041 - 13041-default-widget-latest-presentations (#13153)

---
+++ profile/modules/vsite/modules/vsite_preset/tests/src/ExistingSite/PresentationsDefaultWidgetTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\Tests\vsite_preset\ExistingSite;
+
+use Drupal\Tests\vsite\ExistingSite\VsiteExistingSiteTestBase;
+use Drupal\vsite_preset\Entity\GroupPreset;
+
+/**
+ * NewsDefaultWidgetFunctionalTest.
+ *
+ * @group vsite
+ * @group kernel
+ */
+class PresentationsDefaultWidgetTest extends VsiteExistingSiteTestBase {
+
+  /**
+   * Test group.
+   *
+   * @var \Drupal\group\Entity\GroupInterface
+   */
+  protected $group;
+
+  /**
+   * Vsite helper service.
+   *
+   * @var \Drupal\vsite_preset\Helper\VsitePresetHelper
+   */
+  protected $vsitePresetHelper;
+
+  /**
+   * Entity Type Manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->vsitePresetHelper = $this->container->get('vsite_preset.preset_helper');
+    $this->entityTypeManager = $this->container->get('entity_type.manager');
+    $this->group = $this->createGroup([
+      'type' => 'personal',
+      'field_preset' => 'personal',
+    ]);
+    $this->vsiteContextManager->activateVsite($this->group);
+  }
+
+  /**
+   * Test Faq widget is created.
+   *
+   * @throws \Behat\Mink\Exception\ResponseTextException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function testRecentPresentationsValues() {
+    /** @var \Drupal\vsite_preset\Entity\GroupPreset $preset */
+    $preset = GroupPreset::load('personal');
+    $uriArr = $this->vsitePresetHelper->getCreateFilePaths($preset, $this->group);
+    // Test negative, block content does not exist already.
+    $gid = $this->group->id();
+    $groupStorage = $this->entityTypeManager->getStorage('group_content');
+    $blockArr = $groupStorage->loadByProperties([
+      'gid' => $gid,
+      'label' => 'Recent Presentations',
+    ]);
+    $this->assertEmpty($blockArr);
+    // Retrieve file creation csv source path.
+    foreach ($uriArr as $uri) {
+      $this->vsitePresetHelper->createDefaultContent($this->group, $uri);
+    }
+    // Test positive block content is created.
+    $blockArr = $groupStorage->loadByProperties([
+      'gid' => $gid,
+      'label' => 'Recent Presentations',
+    ]);
+    $blockEntity = array_values($blockArr)[0];
+    $blockEntityId = $blockEntity->entity_id->target_id;
+    // Assert correct field values.
+    $blockContentEntity = $this->entityTypeManager->getStorage('block_content')->load($blockEntityId);
+    $type = $blockContentEntity->get('field_content_type')->value;
+    $displayStyle = $blockContentEntity->get('field_display_style')->value;
+    $sortedBy = $blockContentEntity->get('field_sorted_by')->value;
+    $widgeTitle = $blockContentEntity->get('field_widget_title')->value;
+    $info = $blockContentEntity->get('info')->value;
+    $this->assertEquals('presentation', $type);
+    $this->assertEquals('title', $displayStyle);
+    $this->assertEquals('sort_newest', $sortedBy);
+    $this->assertEquals('Recent Presentations', $widgeTitle);
+    $this->assertEquals('Recent Presentations', $info);
+  }
+
+}
