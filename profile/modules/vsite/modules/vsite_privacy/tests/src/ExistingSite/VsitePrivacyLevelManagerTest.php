6ca550d0d621d8334217d317ef581bb875d15c74
Lomas Rishi Gupta <lomasgupta35@gmail.com>
2020-03-30 10:12:30 +0530

    Issue #13127 - fix incorrect test (#13240)
    * Issue #13127 - 13127-admin-access-test-improvement
    
    * Issue #13127 - 13127-fix for failing test
    
    * Issue #13127 - 13127-admin-access-test-improvement-review-changes

--- profile/modules/vsite/modules/vsite_privacy/tests/src/ExistingSite/VsitePrivacyLevelManagerTest.php
+++ profile/modules/vsite/modules/vsite_privacy/tests/src/ExistingSite/VsitePrivacyLevelManagerTest.php
@@ -20,11 +20,14 @@ class VsitePrivacyLevelManagerTest extends OsExistingSiteTestBase {
   public function test(): void {
     /** @var \Drupal\vsite_privacy\Plugin\VsitePrivacyLevelManagerInterface $vsite_privacy_level_manager */
     $vsite_privacy_level_manager = $this->container->get('vsite.privacy.manager');
-    /** @var \Drupal\Core\Session\AccountInterface $current_user */
+    /** @var \Drupal\vsite\Plugin\VsiteContextManagerInterface $vsite_context_manager */
+    $vsiteContextManager = $this->container->get('vsite.context_manager');
+    $vsiteContextManager->activateVsite($this->group);
+    $vsiteMember = $this->createUser();
+    $vsiteMember->addRole('administrator');
+    $this->assertTrue($vsite_privacy_level_manager->checkAccessForPlugin($vsiteMember, 'private'));
+    $vsiteMember->removeRole('administrator');
     $current_user = $this->container->get('current_user');
-    if ($current_user->hasPermission('administer group')) {
-      $this->assertTrue($vsite_privacy_level_manager->checkAccessForPlugin($current_user, 'private'));
-    }
     $this->assertTrue($vsite_privacy_level_manager->checkAccessForPlugin($current_user, 'public'));
     $this->assertFalse($vsite_privacy_level_manager->checkAccessForPlugin($current_user, 'private'));
   }
